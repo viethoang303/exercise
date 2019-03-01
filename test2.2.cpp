d17d378ef82606940e0e548175a6a2df8cf9e777
viethoang303 <48119382+viethoang303@users.noreply.github.com>
2019-03-01 19:06:23 +0700

    Update test2.2.cpp

--- test2.2.cpp
+++ test2.2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int  factorial(int n)
 {
-    cout << “n = “ << n << “ at ” << &n << endl;
+    cout << "n = " << n << " at " << &n << endl;
     if (n>1) return n*factorial(n-1);
     else if (n==1) return 1;
     else if (n==0) return 1;
