f9d2c2816fccd363cf997e8482abacca29d884b1
viethoang303 <48119382+viethoang303@users.noreply.github.com>
2019-03-01 18:36:15 +0700

    Add files via upload

---
+++ test2.1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int c = 7;
+    int *pc = &c;
+    char d = 'd';
+    char *pd = &d;
+    int a[] = {12,4,6};
+    char b[] = {'t','f','r'};
+    int c1 = 8;
+    char d1 = 'e';
+    int *pa = a;
+    char *pb = b;
+    cout << &pc << " " << &pd << endl;
+    cout << &pa << " " << &pa+1 << " " << &pa+2 << endl;
+    cout << &pb << " " << &pb+1 << " " << &pb+2 << endl;
+}
+// Nhận xét: int chiếm 4 bit, con char chiếm 1 bit.
