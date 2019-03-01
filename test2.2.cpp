3c93088b2058d7935c9e6d3491495712363a7572
viethoang303 <48119382+viethoang303@users.noreply.github.com>
2019-03-01 18:52:02 +0700

    Add files via upload

---
+++ test2.2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int  factorial(int n)
+{
+    if (n>1) return n*factorial(n-1);
+    else if (n==1) return 1;
+    else if (n==0) return 1;
+}
+
+int main ()
+{
+    int n;
+    cin >> n;
+    cout << factorial(n);
+    return 0;
+}
+// Dãy giá trị giảm dần cách đều nhau nên kích thước của 1 stack frame cho hàm là 48 bytes //
