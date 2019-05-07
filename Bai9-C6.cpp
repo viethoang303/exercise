14fa98201edc64586f9674390c2670ab9c5f4044
viethoang303 <48119382+viethoang303@users.noreply.github.com>
2019-05-07 19:28:45 +0700

    Add files via upload

---
+++ Bai9-C6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+char* trim_left(const char* a)
+{
+    int n = strlen(a);
+
+    int k = 0;
+    while (a[k]==' ')
+    {
+        k++;
+    }
+    char* p = new char[n-k];
+    for (int i=k; i<n; i++)
+            p[i-k] = a[i];
+    return p;
+}
+int main()
+{
+    cout << trim_left("   hello  hi");
+    return 0;
+}
